e1907c8868b55660539df273a366770978a3c057
Alex <alexander.ogier@gmail.com>
2022-12-01 18:04:57 +1300

    day 1

--- day1/part1.js
+++ day1/part1.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n').map(g => g.split('\r\n').map(n => parseInt(n)));
 
+var most = Math.max(...input.map(g => g.reduce((a, b) => a + b, 0)));
+
+console.log(most);
--- day1/part2.js
+++ day1/part2.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n').map(g => g.split('\r\n').map(n => parseInt(n)));
 
+var ordered = input.map(g => g.reduce((a, b) => a + b, 0)).sort((a, b) => b - a);
+
+console.log(ordered[0]+ordered[1]+ordered[2]);
