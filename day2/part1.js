0f7dbff9451c9f9a79349b10cdb5f4b34e34e1a5
Alex <alexander.ogier@gmail.com>
2022-12-02 18:16:32 +1300

    day2

--- day2/part1.js
+++ day2/part1.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(i => i.split(' '));
 
+// A for Rock, B for Paper, and C for Scissors
+// X for Rock, Y for Paper, and Z for Scissors
+
+var score = 0;
+
+input.forEach(r => {
+    var value = r[0].charCodeAt(0) - 'A'.charCodeAt(0);
+    var response = r[1].charCodeAt(0) - 'X'.charCodeAt(0);
+
+    // win
+    if (((response + 2) % 3) == value) {
+        score += 6;
+    } else {
+        if (response == value) {
+            // draw
+            score += 3;
+        }
+    }
+    score += (response + 1);
+
+});
+
+console.log(score);
--- day2/part2.js
+++ day2/part2.js
@@ -1,4 +1,40 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(i => i.split(' '));
 
+// A for Rock, B for Paper, and C for Scissors
+// X for Rock, Y for Paper, and Z for Scissors
+
+var score = 0;
+
+input.forEach(r => {
+    var value = r[0].charCodeAt(0) - 'A'.charCodeAt(0);
+
+    var response = 0;
+
+    switch (r[1]) {
+        case 'X':
+            response = (value + 2) % 3;
+            break;
+        case 'Y':
+            response = value;
+            break;
+        case 'Z':
+            response = (value + 1) % 3;
+            break;
+    }
+
+    // win
+    if (((response + 2) % 3) == value) {
+        score += 6;
+    } else {
+        if (response == value) {
+            // draw
+            score += 3;
+        }
+    }
+    score += (response + 1);
+
+});
+
+console.log(score);
