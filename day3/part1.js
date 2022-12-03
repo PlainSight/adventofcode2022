0fb3db4615d6409f56b3fe88fb02c87c16dbe812
Alex <alexander.ogier@gmail.com>
2022-12-03 18:19:38 +1300

    day3

--- day3/part1.js
+++ day3/part1.js
@@ -2,3 +2,17 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var sum = 0;
+
+var most = input.map(i => {
+    var dupe = null;
+    var first = i.slice(0, i.length / 2).split('').reduce((a, c) => { a[c] = true; return a}, {});
+    i.slice(i.length / 2).split('').forEach(n => { if (first[n]) { dupe = n } });
+    if (dupe.toLowerCase() == dupe) {
+        sum += dupe.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    } else {
+        sum += dupe.charCodeAt(0) - 'A'.charCodeAt(0) + 27;
+    }
+})
+
+console.log(sum);
--- day3/part2.js
+++ day3/part2.js
@@ -2,3 +2,20 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var sum = 0;
+
+input.reduce((a, c, i) => { a[Math.floor(i/3)] = a[Math.floor(i/3)] || []; a[Math.floor(i/3)].push(c); return a; }, []).map(i => {
+    var counts = {};
+    i[0].split('').forEach(n => { counts[n] = counts[n] || 0; counts[n] = counts[n] | 1; });
+    i[1].split('').forEach(n => { counts[n] = counts[n] || 0; counts[n] = counts[n] | 2; });
+    i[2].split('').forEach(n => { counts[n] = counts[n] || 0; counts[n] = counts[n] | 4; });
+    var entry = Object.entries(counts).filter(e => e[1] == 7);
+    var dupe = entry[0][0];
+    if (dupe.toLowerCase() == dupe) {
+        sum += dupe.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    } else {
+        sum += dupe.charCodeAt(0) - 'A'.charCodeAt(0) + 27;
+    }
+})
+
+console.log(sum);
