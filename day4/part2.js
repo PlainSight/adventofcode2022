f23c6a5e7d9ae001ff2eae8e229601f7af23a543
Alex <alexander.ogier@gmail.com>
2022-12-04 18:16:32 +1300

    day4

--- day4/part2.js
+++ day4/part2.js
@@ -2,3 +2,26 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var count = 0;
+
+input.forEach(i => {
+    var ranges = i.split(',').map(n => n.split('-').map(x => parseInt(x)));
+
+    if (ranges[0][1] <= ranges[1][1] && ranges[0][0] >= ranges[1][0]) {
+        count++;
+    } else {
+        if (ranges[1][1] <= ranges[0][1] && ranges[1][0] >= ranges[0][0]) {
+            count++;
+        } else {
+            if (ranges[0][0] <= ranges[1][1] && ranges[1][1] <= ranges[0][1]) {
+                count++;
+            } else {
+                if (ranges[0][0] <= ranges[1][0] && ranges[1][0] <= ranges[0][1]) {
+                    count++;
+                }
+            }
+        }
+    }
+})
+
+console.log(count);
