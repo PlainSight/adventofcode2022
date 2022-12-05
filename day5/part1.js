5367199b705fce8b6776e4b71723e8d204b78ddc
Alex <alexander.ogier@gmail.com>
2022-12-05 18:13:31 +1300

    day5

--- day5/part1.js
+++ day5/part1.js
@@ -1,4 +1,38 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
 
+var columns = [];
+
+var stacks = input[0].split('\r\n');
+
+stacks.map((r, ii) => {
+    if (ii == stacks.length - 1) {
+        return;
+    }
+    for(var i = 0; i < r.length; i++) {
+        if (/[a-zA-Z]/.test(r.charAt(i))) {
+            var num = 1 + Math.floor(i/4);
+            columns[num] = (columns[num] || []);
+            columns[num].unshift(r.charAt(i));
+        }
+    }
+});
+
+var instructions = input[1].split('\r\n');
+
+instructions.forEach(i => {
+    var parts = /move (\d+) from (\d+) to (\d+)/.exec(i);
+    var q = parseInt(parts[1]);
+    var f = parseInt(parts[2]);
+    var t = parseInt(parts[3]);
+
+    for(var c = 0; c < q; c++) {
+        var moving = columns[f].pop();
+        columns[t].push(moving);
+    }
+});
+
+var res = columns.map(c => c.pop()).join('');
+
+console.log(res);
--- day5/part2.js
+++ day5/part2.js
@@ -1,4 +1,45 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
 
+var columns = [];
+
+var stacks = input[0].split('\r\n');
+
+stacks.map((r, ii) => {
+    if (ii == stacks.length - 1) {
+        return;
+    }
+    for(var i = 0; i < r.length; i++) {
+        if (/[a-zA-Z]/.test(r.charAt(i))) {
+            var num = 1 + Math.floor(i/4);
+            columns[num] = (columns[num] || []);
+            columns[num].unshift(r.charAt(i));
+        }
+    }
+});
+
+
+var instructions = input[1].split('\r\n');
+
+instructions.forEach(i => {
+    var parts = /move (\d+) from (\d+) to (\d+)/.exec(i);
+    var q = parseInt(parts[1]);
+    var f = parseInt(parts[2]);
+    var t = parseInt(parts[3]);
+
+    var stack = [];
+
+    for(var c = 0; c < q; c++) {
+        var moving = columns[f].pop();
+        stack.push(moving);
+    }
+
+    for(var c = 0; c < q; c++) {
+        columns[t].push(stack.pop());
+    }
+});
+
+var res = columns.map(c => c.pop()).join('');
+
+console.log(res);
