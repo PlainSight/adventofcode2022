3c052bf5abe5b4d287beff7c526f56786069ec9e
Alex <alexander.ogier@gmail.com>
2022-12-06 18:08:42 +1300

    day6

--- day6/part1.js
+++ day6/part1.js
@@ -1,4 +1,28 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8');
 
+var last4 = [];
+
+var first = -1;
+
+input.split('').forEach((c, i) => {
+    if (last4.length == 4) {
+        last4.push(c);
+        last4.shift();
+    } else {
+        last4.push(c);
+    }
+
+    if (last4.length == 4) {
+        var distinct = last4.reduce((a, c) => {
+            a[c] = true;
+            return a;
+        }, {});
+        if (Object.values(distinct).length == 4 && first == -1) {
+            first = i;
+        }
+    }
+});
+
+console.log(first+1);
