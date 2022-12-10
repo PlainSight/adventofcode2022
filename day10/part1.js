9c7bc0366b269097f0d29a26711d77298c5e1f7c
Alex <alexander.ogier@gmail.com>
2022-12-10 18:30:21 +1300

    day 10

--- day10/part1.js
+++ day10/part1.js
@@ -2,3 +2,48 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var X = 1;
+var cycle = 1;
+
+var sum = 0;
+
+function isCycleValue(c) {
+    var val = c - 20;
+    return val % 40 == 0;
+}
+
+input.forEach(i => {
+    var parts = i.split(' ');
+
+    var cycleValue = 0;
+    var xValue = 0;
+
+    switch (parts[0]) {
+        case 'addx':
+            if (isCycleValue(cycle+1)) {
+                cycleValue = cycle+1;
+                xValue = X;
+            }
+            cycle += 2;
+            X += parseInt(parts[1]);
+            if (isCycleValue(cycle)) {
+                cycleValue = cycle;
+                xValue = X;
+            }
+        break;
+        case 'noop':
+            cycle += 1;
+            if (isCycleValue(cycle)) {
+                cycleValue = cycle;
+                xValue = X;
+            }
+        break;
+    }
+
+    if (cycleValue) {
+        sum += (xValue * cycleValue);
+    }
+})
+
+
+console.log(sum);
--- day10/part2.js
+++ day10/part2.js
@@ -2,3 +2,41 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
 
+var X = 1;
+var cycle = 1;
+
+var screen = [[],[],[],[],[],[]];
+
+var x = 0;
+var y = 0;
+
+input.forEach(i => {
+    var parts = i.split(' ');
+
+    function incCycle() {
+        if (Math.abs(x - X) < 2) {
+            screen[y][x] = '#';
+        } else {
+            screen[y][x] = ' ';
+        }
+        x++;
+        if (x >= 40) {
+            x = 0;
+            y++;
+        }
+        cycle++;
+    }
+
+    switch (parts[0]) {
+        case 'addx':
+            incCycle();
+            incCycle();
+            X += parseInt(parts[1]);
+        break;
+        case 'noop':
+            incCycle();
+        break;
+    }
+})
+
+console.log(screen.map(r => r.join('')).join('\r\n'));
