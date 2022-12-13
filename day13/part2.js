05dae231ad871a9e7928aea5240193cee93d852e
Alex <alexander.ogier@gmail.com>
2022-12-13 19:38:30 +1300

    day 13

--- day13/part2.js
+++ day13/part2.js
@@ -1,4 +1,50 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var all = fs.readFileSync('./input.txt', 'utf8').split('\r\n').filter(l => l != '');
 
+var div1 = '[[2]]';
+var div2 = '[[6]]';
+
+all.push(div1);
+all.push(div2);
+
+function compare(l, r) {
+    if (!Array.isArray(l) && !Array.isArray(r)) {
+        return l < r ? 1 : (r < l ? -1 : 0);
+    } else {
+        if (Array.isArray(l) && Array.isArray(r)) {
+            for(var i = 0; i < l.length && i < r.length; i++) {
+                var res = compare(l[i], r[i]);
+                if (res != 0) {
+                    return res;
+                }
+            }
+            if (l.length < r.length) {
+                return 1;
+            } else {
+                if (r.length < l.length) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        } else {
+            if (Array.isArray(l)) {
+                return compare(l, [r])
+            } else {
+                return compare([l], r)
+            }
+        }
+    }
+}
+
+all.sort((a, b) => {
+    var a1 = JSON.parse(a);
+    var b1 = JSON.parse(b);
+
+    return compare(b1, a1);
+})
+
+var val = (all.indexOf(div1)+1)*(all.indexOf(div2)+1);
+
+console.log(val);
